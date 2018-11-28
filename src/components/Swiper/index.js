8e5c7ad0c7416eba840e0ac7f1b0048bf4807977
丁效辉 <dxhuii@qq.com>
2018-11-29 02:08:01 +0800

    再次更新swiper

--- src/components/Swiper/index.js
+++ src/components/Swiper/index.js
@@ -1,36 +1,35 @@
 import React, { Component, Fragment } from 'react'
-import { Swipe } from '../../utils/swipe.min'
+import { Swipe, SwipeItem } from 'swipejs/react'
 import './style.scss'
 
-class SwiperMod extends Component {
+export default class Swiper extends Component {
   constructor(props) {
     super(props)
-    this.mySwipe = null
+    this.state = {}
+    this.mySwipe = {}
+    this.bullets = []
   }
-
   componentDidMount() {
+    this.mySwipe = { ...this.swipe.instance }
     const pager = document.querySelector('.pager')
-    const bullets = pager.querySelectorAll('em')
+    this.bullets = pager.querySelectorAll('em')
     const that = this
-    this.mySwipe = new Swipe(document.querySelector('.swipe'), {
-      startSlide: 0,
-      speed: 400,
-      auto: 3000,
-      continuous: !0,
-      disableScroll: !1,
-      stopPropagation: !1,
-      callback: function(index) {
-        that.slideTab(index, bullets)
-      }
-    })
 
-    for (let i = 0; i < bullets.length; i++) {
-      var elem = bullets[i]
+    for (let i = 0; i < this.bullets.length; i++) {
+      var elem = this.bullets[i]
       elem.setAttribute('data-tab', i)
       elem.onclick = function() {
         that.mySwipe.slide(parseInt(this.getAttribute('data-tab'), 10), 500)
       }
     }
+  }
+
+  handleCallback = index => {
+    this.slideTab(index, this.bullets)
+  }
+
+  onTransactionEnd = () => {
+    this.mySwipe.restart()
   }
 
   slideTab(index, bullets) {
@@ -49,22 +48,30 @@ class SwiperMod extends Component {
     this.mySwipe.next()
   }
 
+  handleClick = e => {
+    // Your own logic
+  }
+
   render() {
     return (
       <Fragment>
-        <div className="swipe">
-          <div className="swipe-wrap">
-            <div>
-              <b>0</b>
-            </div>
-            <div>
-              <b>1</b>
-            </div>
-            <div>
-              <b>2</b>
-            </div>
-          </div>
-        </div>
+        <Swipe
+          ref={o => (this.swipe = o)}
+          startSlide={0}
+          speed={300}
+          auto={3000}
+          draggable={true}
+          continuous={true}
+          autoRestart={false}
+          disableScroll={false}
+          stopPropagation={false}
+          callback={this.handleCallback}
+          transitionEnd={this.onTransactionEnd}
+        >
+          <SwipeItem onClick={this.handleClick}>Slide One</SwipeItem>
+          <SwipeItem onClick={this.handleClick.bind(this)}>Slide Two</SwipeItem>
+          <SwipeItem onClick={this.handleClick.bind(this)}>Slide Three</SwipeItem>
+        </Swipe>
         <div style={{ textAlign: 'center', paddingTop: 20 }}>
           <div className="pager">
             <em className="on">1</em>
@@ -78,5 +85,3 @@ class SwiperMod extends Component {
     )
   }
 }
-
-export default SwiperMod
