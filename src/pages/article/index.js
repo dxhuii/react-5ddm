2f638d09f767c2e2c3d19bdb3ee0a0e970880d1f
丁效辉 <dxhuii@qq.com>
2018-12-21 20:28:19 +0800

    优化新闻样式

--- src/pages/article/index.js
+++ src/pages/article/index.js
@@ -52,6 +52,8 @@ class Article extends Component {
     const {
       title,
       id,
+      name,
+      cid,
       pic,
       remark,
       keywords,
@@ -68,58 +70,73 @@ class Article extends Component {
     } = data
     const playHtml = isJump(playurl, playname, 1)
     return (
-      <div className="warp-bg">
-        <Meta title={title}>
-          <meta property="og:locale" content="zh_CN" />
-          <meta property="og:type" content="article" />
-          <meta property="og:title" content={title} />
-          <meta property="og:description" content={remark} />
-          {pic ? <meta property="og:image" content={pic} /> : null}
-          <meta property="og:url" content={`/article/${id}`} />
-          <meta property="og:site_name" content="9站" />
-          <meta name="description" content={remark} />
-          {keywords ? <meta name="keywords" content={keywords} /> : null}
-        </Meta>
-        <div className="wp clearfix" styleName="article">
-          <div styleName="article-left">
-            <article styleName="article-body">
+      <div className="wp mt20 clearfix" styleName="article">
+        {playname ? (
+          <Meta title={title}>
+            <meta property="og:locale" content="zh_CN" />
+            <meta property="og:type" content="article" />
+            <meta property="og:title" content={title} />
+            <meta property="og:description" content={remark} />
+            <meta property="og:url" content={`/article/${id}`} />
+            <meta property="og:site_name" content="9站" />
+            <meta name="description" content={remark} />
+            <meta name="keywords" content={keywords} />
+          </Meta>
+        ) : (
+          <Meta title={title}>
+            <meta property="og:locale" content="zh_CN" />
+            <meta property="og:type" content="article" />
+            <meta property="og:title" content={title} />
+            <meta property="og:description" content={remark} />
+            <meta property="og:image" content={pic} />
+            <meta property="og:url" content={`/article/${id}`} />
+            <meta property="og:site_name" content="9站" />
+            <meta name="description" content={remark} />
+            <meta name="keywords" content={keywords} />
+          </Meta>
+        )}
+        <div styleName="article-left">
+          <article styleName="article-body">
+            <div styleName="article-head">
               <h1>{title}</h1>
               <div styleName="article-label">
                 <span>来源：{inputer ? inputer : '网络'}</span>
                 <span>更新时间：{addtime}</span>
               </div>
-              {playname ? (
-                <div styleName="article-video" dangerouslySetInnerHTML={{ __html: playHtml }} />
-              ) : (
-                <div styleName="article-content" dangerouslySetInnerHTML={{ __html: content }} />
-              )}
+            </div>
 
-              <div styleName="article-tool" className="mt20">
-                {tag.length > 0 ? (
-                  <div styleName="article-tool__tag">
-                    {tag.map(item => (
-                      <span key={item}>{item}</span>
-                    ))}
-                  </div>
-                ) : null}
-                <div styleName="article-tool__share" />
-              </div>
-              <div styleName="article-context" className="mt20">
-                {prev ? (
-                  <p>
-                    <Link to={`/artilce/${prev.id}`}>{prev.title}</Link>
-                  </p>
-                ) : null}
-                {next ? (
-                  <p>
-                    <Link to={`/artilce/${next.id}`}>{next.title}</Link>
-                  </p>
-                ) : null}
-              </div>
-            </article>
-          </div>
-          <div styleName="article-right">right</div>
+            {remark ? <div styleName="acticle-remark">{remark}</div> : null}
+            {playname ? (
+              <div styleName="article-video" dangerouslySetInnerHTML={{ __html: playHtml }} />
+            ) : (
+              <div styleName="article-content" dangerouslySetInnerHTML={{ __html: content }} />
+            )}
+
+            <div styleName="article-tool" className="mt20">
+              {tag.length > 0 ? (
+                <div styleName="article-tool__tag">
+                  {tag.map(item => (
+                    <span key={item}>{item}</span>
+                  ))}
+                </div>
+              ) : null}
+              <div styleName="article-tool__share" />
+            </div>
+            <div styleName="article-context" className="mt20">
+              {prev ? (
+                <p>
+                  <Link to={`/article/${prev.id}`}>{prev.title}</Link>
+                </p>
+              ) : null}
+              {next ? (
+                <p>
+                  <Link to={`/article/${next.id}`}>{next.title}</Link>
+                </p>
+              ) : null}
+            </div>
+          </article>
         </div>
+        <div styleName="article-right">right</div>
       </div>
     )
   }
