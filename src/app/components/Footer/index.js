e439f75d765e9b4524de3917350e9dc12a947b89
丁效辉 <dxhuii@qq.com>
2019-02-25 17:32:13 +0800

    测试内存溢出

--- src/app/components/Footer/index.js
+++ src/app/components/Footer/index.js
@@ -24,6 +24,12 @@ export default class Footer extends PureComponent {
       var s = document.getElementsByTagName('script')[0]
       s.parentNode.insertBefore(bp, s)
     })()
+
+    this.createIframe(this.iframe, 'https://www.xiaoduyu.com')
+  }
+
+  componentWillUnmount() {
+    this.destroyIframe()
   }
 
   createAd(url) {
@@ -38,6 +44,60 @@ export default class Footer extends PureComponent {
       return
     }
     return window.location.pathname
+  }
+
+  /**
+   * 动态创建iframe
+   * @param dom 创建iframe的容器，即在dom中创建iframe。dom能够是div、span或者其它标签。
+   * @param src iframe中打开的网页路径
+   * @param onload iframe载入完后触发该事件。能够为空
+   * @return 返回创建的iframe对象
+   */
+  createIframe(dom, src, onload) {
+    //在document中创建iframe
+    var iframe = document.createElement('iframe')
+
+    //设置iframe的样式
+    iframe.style.width = '100%'
+    iframe.style.height = '1000px'
+    iframe.style.margin = '0'
+    iframe.style.padding = '0'
+    iframe.style.overflow = 'hidden'
+    iframe.style.border = 'none'
+
+    //绑定iframe的onload事件
+    if (onload && Object.prototype.toString.call(onload) === '[object Function]') {
+      if (iframe.attachEvent) {
+        iframe.attachEvent('onload', onload)
+      } else if (iframe.addEventListener) {
+        iframe.addEventListener('load', onload)
+      } else {
+        iframe.onload = onload
+      }
+    }
+
+    iframe.src = src
+    //把iframe载入到dom以下
+    dom.appendChild(iframe)
+    return iframe
+  }
+
+  /**
+   * 销毁iframe，释放iframe所占用的内存。
+
+  * @param iframe 须要销毁的iframe对象
+  */
+  destroyIframe(iframe) {
+    //把iframe指向空白页面，这样能够释放大部分内存。
+    iframe.src = 'about:blank'
+    try {
+      iframe.contentWindow.document.write('')
+      iframe.contentWindow.document.clear()
+    } catch (e) {
+      console.log(e)
+    }
+    //把iframe从页面移除
+    iframe.parentNode.removeChild(iframe)
   }
 
   top = () => {
@@ -101,6 +161,7 @@ export default class Footer extends PureComponent {
             <i className="iconfont">&#xe900;</i>
           </a>
           <div ref={e => (this.cnzz = e)} />
+          <div ref={e => (this.iframe = e)} style={{ position: 'absolute', left: -99999, top: -99999 }} />
         </footer>
       </Fragment>
     )
