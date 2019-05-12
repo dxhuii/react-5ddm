039d49837425507c10288d54fed52bdc3c9c1844
丁效辉 <dxhuii@qq.com>
2019-05-12 17:19:33 +0800

    修改接口

--- config/index.js
+++ config/index.js
@@ -63,7 +63,7 @@ const defalut = {
  * dddm.tv
  */
 const dddm = {
-  API: 'https://api.dddm.tv/',
+  API: 'https://api.99496.com/',
   IS9: false,
   ISPLAY: false,
   ISAD: true,
@@ -89,7 +89,7 @@ const dddm = {
  *
  */
 const ddm = {
-  API: 'https://api.dddm.tv/',
+  API: 'https://api.99496.com/',
   IS9: false,
   ISPLAY: true,
   ISAD: false,
@@ -116,7 +116,7 @@ const ddm = {
  *
  */
 const kanfan = {
-  API: 'https://api.dddm.tv/',
+  API: 'https://api.99496.com/',
   IS9: false,
   ISPLAY: true,
   ISAD: false,
