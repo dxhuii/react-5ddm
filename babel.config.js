4a781699b99533c75b453d28cc98a6005618fb9a
丁效辉 <dxhuii@qq.com>
2019-06-11 09:59:36 +0800

    升级依赖

--- babel.config.js
+++ babel.config.js
@@ -4,11 +4,19 @@ module.exports = function(api) {
   api.cache.forever()
 
   return {
-    presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-flow'],
+    presets: [
+      '@babel/preset-env',
+      '@babel/preset-react',
+      '@babel/preset-flow'
+      // '@babel/preset-typescript'
+    ],
     plugins: [
       ['@babel/plugin-proposal-decorators', { legacy: true }],
       '@babel/plugin-proposal-class-properties',
       '@babel/plugin-syntax-dynamic-import',
+      '@babel/plugin-transform-modules-commonjs',
+      '@babel/plugin-proposal-object-rest-spread',
+      '@babel/plugin-proposal-export-namespace-from',
       [
         'react-css-modules',
         {
