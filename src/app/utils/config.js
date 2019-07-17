fcd3724ba160941983249f8f64c8b0ba575dd76a
丁效辉 <dxhuii@qq.com>
2019-07-17 21:55:25 +0800

    修改游戏接口

--- src/app/utils/config.js
+++ src/app/utils/config.js
@@ -49,6 +49,6 @@ export default {
     emptyhistory: `${apiReact}emptyhistory`, // 清空观看记录 需登录
     delplaylog: `${apiReact}delplaylog`, // 删除观看记录 需登录
     // 游戏
-    game: `https://api.99496.com/r.php?s=home-react-game` // order排序 wd 关键词 p 分页 limit 每页条数
+    game: `${apiReact}game` // order排序 wd 关键词 p 分页 limit 每页条数
   }
 }
